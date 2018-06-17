fb29268375b0e81cc0480e5ff1f6969ff8e6ebcb
thuy <thuy.ntrinh@gmail.com>
2018-06-17 02:23:28 +0200

    Roll back to buggy impl

--- domain/src/test/java/thuy/weakrefmisuse/Subscriber.kt
+++ domain/src/test/java/thuy/weakrefmisuse/Subscriber.kt
@@ -3,14 +3,15 @@ package thuy.weakrefmisuse
 class Subscriber(publisher: Publisher) {
   var events: List<Event> = emptyList()
 
-  private val callback: Callback<Event> = object : Callback<Event> {
-    override fun invoke(event: Event) {
-      events += event
-      println("Received ⟵ $event")
-    }
-  }
-
   init {
     publisher.callback = callback
   }
+
+  private val callback: Callback<Event>
+    get() = object : Callback<Event> {
+      override fun invoke(event: Event) {
+        events += event
+        println("Received ⟵ $event")
+      }
+    }
 }
